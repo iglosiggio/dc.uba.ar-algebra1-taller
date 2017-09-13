ef9c86471500a7d45b0925318662ca22139196d1
Ignacio E. Losiggio <iglosiggio@dc.uba.ar>
2017-09-13 19:00:30 -0300

    agregado e

--- clase05.hs
+++ clase05.hs
@@ -4,6 +4,8 @@ eAprox n = eAprox' n 0
 eAprox' 0 acc = acc + 1
 eAprox' n acc = eAprox' (n - 1) (acc + 1 / fromInteger (fact n))
 
+e = eAprox 100
+
 fact 0 = 1
 fact n = fact' n 1
 fact' 0 acc = acc

637ddae6d9b03f40e265ff749525ce1f027c5118
Ignacio E. Losiggio <iglosiggio@dc.uba.ar>
2017-09-13 18:12:26 -0300

    Uso TCR en eAprox y fact

--- clase05.hs
+++ clase05.hs
@@ -1,9 +1,10 @@
 eAprox :: Integer -> Float
 eAprox 0 = 1
-eAprox x = eAprox (x - 1) + 1 / fact (fromInteger x)
+eAprox n = eAprox' n 0
+eAprox' 0 acc = acc + 1
+eAprox' n acc = eAprox' (n - 1) (acc + 1 / fromInteger (fact n))
 
 fact 0 = 1
-fact n = n * fact (n - 1)
-
+fact n = fact' n 1
 fact' 0 acc = acc
 fact' n acc = fact' (n - 1) (acc * n)

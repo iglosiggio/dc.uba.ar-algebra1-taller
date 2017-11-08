a8c23645dbff7271b8676356943632e201cec14f
Ignacio E. Losiggio <iglosiggio@dc.uba.ar>
2017-11-08 19:43:14 -0300

    Empezando a hacer el tp

--- tp/Aritmetica.hs
+++ tp/Aritmetica.hs
@@ -10,11 +10,28 @@ mcdExt _ _ = (0, (0, 0))
 
 --(2)
 criba :: Integer -> Set Integer
-criba _ = []
+criba n = cribaDesde 2 n []
+
+cribaDesde :: Integer -> Integer -> Set Integer -> Set Integer
+cribaDesde desde hasta primos | desde == hasta = primos
+    | desde < hasta && loDivideAlguno desde primos = cribaDesde (desde + 1) hasta primos
+    | desde < hasta = cribaDesde (desde + 1) hasta (desde:primos)
+
+loDivideAlguno :: Integer -> Set Integer -> Bool
+loDivideAlguno _ [] = False
+loDivideAlguno n (x:xs) = esDivisor x n || loDivideAlguno n xs
+
+filtrarDivisores :: Integer -> Set Integer -> Bool
+filtrarDivisores n [] = []
+filtrarDivisores n (x:xs) | esDivisor x n = filtrarDivisores n xs
+                          | otherwise = x : filtrarDivisores n xs
+
+esDivisor :: Integer -> Integer -> Bool
+esDivisor d n = mod n d == 0
 
 --(3)
 coprimoCon:: Integer -> Integer
-coprimoCon _ = 0
+coprimoCon n = buscarCoprimo n filtrarDivisores n (criba n)
 
 
 --(4)

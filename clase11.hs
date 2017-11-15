cd82e9262e4d61391ea79f59239895dcb1115368
Ignacio E. Losiggio <iglosiggio@dc.uba.ar>
2017-11-15 18:39:07 -0300

    Agregadas cosas de la clase 10

---
+++ clase11.hs
@@ -0,0 +1,18 @@
+fold :: (a -> b -> b) -> b -> [a] -> b
+fold f acc [] = acc
+fold f acc (x:xs) = f x (fold f acc xs)
+
+tamañoLista :: [a] -> Integer
+tamañoLista = (fold (\_ -> (+1)) 0)
+
+sumar :: Num a => [a] -> a
+sumar = (fold (+) 0)
+
+transformar :: (a -> b) -> [a] -> [b]
+transformar f = (fold transformarElemento [])
+  where transformarElemento v lista = f v : lista
+
+filtrar :: (a -> Bool) -> [a] -> [a]
+filtrar f = (fold chequear [])
+  where chequear v acc | f v       = v : acc
+                       | otherwise = acc

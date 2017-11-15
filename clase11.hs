17523c490cfe0dab74a3ab88f0c753b06b755c8c
Ignacio E. Losiggio <iglosiggio@dc.uba.ar>
2017-11-15 18:58:50 -0300

    ajfksjdgiewjf

--- clase11.hs
+++ clase11.hs
@@ -16,3 +16,14 @@ filtrar :: (a -> Bool) -> [a] -> [a]
 filtrar f = (fold chequear [])
   where chequear v acc | f v       = v : acc
                        | otherwise = acc
+
+--criba :: Integer -> [Integer]
+--criba 1 = []
+--criba n = [p | p <= [1..n], ]
+
+--factorizar :: Integer -> [(Integer, Integer)]
+--factorizar n = [(p, valuacion p n) | p <- criba n, mod n p == 0]
+
+--valuacion :: Integer -> Integer -> Integer
+--valuacion p n | mod n p == 0 = 1 + valuacion p (div n p)
+--              | otherwise    = 0
